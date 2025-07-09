dc00943da4af250fdd0da22ce356714321cea609
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-07-09 18:03:29 -0400

    fixed ignore config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    "host": "your_host",
-    "user": "your_username",
-    "password": "your_password",
-    "database": "your_db"
-}
-
-RAW_DATA_PATH = "data_ingest/legacy_weekly_sales_export.csv"
-CLEAN_TABLE_NAME = "cleaned_sales"
